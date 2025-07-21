673babd6108a098cbf5fcbb0944a5ffcf72ae8a5
Daksh Tekwani <daksh.4419051924@std.ggsipu.ac.in>
2025-07-21 20:56:25 +0530

    Update Task 1.py

--- Task 1.py
+++ Task 1.py
@@ -1,5 +1,8 @@
 a = int(input("Enter the first number : "))
 b = int(input("Enter the second number ; "))
+
+print("\n")
+
 print("Addition : ", a+b)
 print("Subtraction : ", a-b)
 print("Multiplication : ", a*b)
