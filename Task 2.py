386850fcb6d5327f06b95c20a2a9a98c94a31ead
Daksh Tekwani <daksh.4419051924@std.ggsipu.ac.in>
2025-07-21 21:03:01 +0530

    Create Task 2.py

---
+++ Task 2.py
@@ -0,0 +1,4 @@
+first_name = input("Enter your first name : ")
+second_name = input("Enter your second name : ")
+messege = "Hello, " + first_name + " " + second_name + "! Welcome to the PYTHON program"
+print(messege)
