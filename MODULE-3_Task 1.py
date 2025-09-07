3299abd9f726f29c3d726e1401a33d9cff0008f4
Daksh Tekwani <daksh.4419051924@std.ggsipu.ac.in>
2025-09-07 18:48:00 +0530

    Update MODULE-3_Task 1.py

--- MODULE-3_Task 1.py
+++ MODULE-3_Task 1.py
@@ -1,7 +1,7 @@
 num = int(input("Enter an integer: "))
 
 if num % 2 == 0:
-    print(f"{num} is an Even number.")
+    print(f"{num} is an EVEN number.")
 else:
-    print(f"{num} is an Odd number.")
+    print(f"{num} is an ODD number.")
   
