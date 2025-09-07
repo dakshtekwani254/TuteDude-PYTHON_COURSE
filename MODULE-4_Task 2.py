5f6c40a8e6c6ff883da0d644829a5228a3afb87c
Daksh Tekwani <daksh.4419051924@std.ggsipu.ac.in>
2025-09-07 17:20:21 +0530

    Create MODULE-4_Task 2.py

---
+++ MODULE-4_Task 2.py
@@ -0,0 +1,11 @@
+import math
+
+a = float(input("Enter a number: "))
+
+sqrt = math.sqrt(a)
+log = math.log(a)
+sin = math.sin(a)
+
+print(f"Square root of {a} = {sqrt}")
+print(f"Natural logarithm of {a} = {log}")
+print(f"Sine of {a} radians = {sin}")
