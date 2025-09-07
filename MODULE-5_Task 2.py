cb6568ef7f768fbd1d3dcb98015ac3141105a843
Daksh Tekwani <daksh.4419051924@std.ggsipu.ac.in>
2025-09-07 18:45:19 +0530

    Create MODULE-5_Task 2.py

---
+++ MODULE-5_Task 2.py
@@ -0,0 +1,16 @@
+filename = "output.txt"
+
+text_to_write = input("Enter text to write to the file: ")
+with open(filename, "w") as file:
+    file.write(text_to_write + "\n")
+print(f"Data successfully written to {filename}.\n")
+
+text_to_append = input("Enter additional text to append: ")
+with open(filename, "a") as file:
+    file.write(text_to_append + "\n")
+print("Data successfully appended.\n")
+
+print(f"Final content of {filename}:")
+with open(filename, "r") as file:
+    for line in file:
+        print(line.strip())
