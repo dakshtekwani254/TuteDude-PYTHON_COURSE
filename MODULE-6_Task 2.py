df1bb36cae5243976ecc8a5617b62ef55d8b3f39
Daksh Tekwani <daksh.4419051924@std.ggsipu.ac.in>
2025-09-07 18:55:50 +0530

    Create MODULE-6_Task 2.py

---
+++ MODULE-6_Task 2.py
@@ -0,0 +1,9 @@
+num = list(range(1, 11))
+
+first = num[:5]
+
+reversed = first[::-1]
+
+print("ORIGNAL list:", num)
+print("FIRST-FIVE elements:", first)
+print("REVERSED first-five elements:", reversed)
