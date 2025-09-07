da5102de010be90940de30115153ea47a9b564d9
Daksh Tekwani <daksh.4419051924@std.ggsipu.ac.in>
2025-09-07 17:13:22 +0530

    Update MODULE-2_Task 2.py

--- MODULE-2_Task 2.py
+++ MODULE-2_Task 2.py
@@ -1,4 +1,6 @@
 first_name = input("Enter your first name : ")
 second_name = input("Enter your second name : ")
+
 messege = "Hello, " + first_name + " " + second_name + "! Welcome to the PYTHON program"
+
 print(messege)
